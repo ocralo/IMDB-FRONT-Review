2d6b8c6c2a3b869d9c35bdfd0c5b84c35e23c572
ocralo <ocralo@gmail.com>
2020-08-31 23:16:00 -0500

    se crea la card de prueba para la vista home

--- src/Components/Components/CardSerie/CardSerie.js
+++ src/Components/Components/CardSerie/CardSerie.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+
+//importacion de librerias externas
+import { Card } from "react-bootstrap";
 
 /**
  * Card de series
  */
 export default function CardSerie() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<Card className="shadow mt-4">
+			<Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/7XEtHQoy1hwa8XWaOkSv3rlteea.jpg" />
+			<Card.Body className="p-2">
+				<Card.Title className="m-0">Card Title</Card.Title>
+			</Card.Body>
+		</Card>
+	);
 }
