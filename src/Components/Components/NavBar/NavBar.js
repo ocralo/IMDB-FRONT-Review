834ba1991f889da8796cc45f985fbc669d421523
ocralo <ocralo@gmail.com>
2020-08-31 22:23:04 -0500

    Se crea el compoenete Navbar

---
+++ src/Components/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//importacion de librerias externas
+
+//importacion de componenetes
+
+/**
+ * componente Navbar, el cual sera el menu superior de la aplicacion
+ */
+export default function NavBar() {
+	return <div></div>;
+}
