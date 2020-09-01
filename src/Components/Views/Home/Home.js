f3a41fc6ba11757608794b6f6f77b9d20bdcc5d6
ocralo <ocralo@gmail.com>
2020-08-31 23:28:43 -0500

    -Se configura la maquetacion de -Se agrega la card de prueba a la vista home -correccion de titulo en el componente card

--- src/Components/Views/Home/Home.js
+++ src/Components/Views/Home/Home.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from "react";
+
+//importacion de librerias externas
+import { Container, Row, Col } from "react-bootstrap";
+
+//importacion de componentes
+import CardSerie from "../../Components/CardSerie/CardSerie";
 
 /**
  * Vista principal de la aplicacion
  */
 export default function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<Container className="mt-4">
+			<Row>
+				<Col>
+					<h1>Series</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+				<Col xs={12} md={4} sm={6}>
+					<CardSerie />
+				</Col>
+			</Row>
+		</Container>
+	);
 }
