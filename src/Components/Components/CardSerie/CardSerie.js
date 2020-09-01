a0985d603543e36ec0edf2e1aac96eb4d2179072
ocralo <ocralo@gmail.com>
2020-08-31 23:33:08 -0500

    se da formato a CardSerie

--- src/Components/Components/CardSerie/CardSerie.js
+++ src/Components/Components/CardSerie/CardSerie.js
@@ -9,7 +9,10 @@ import { Card } from "react-bootstrap";
 export default function CardSerie() {
 	return (
 		<Card className="shadow mt-4">
-			<Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/7XEtHQoy1hwa8XWaOkSv3rlteea.jpg" />
+			<Card.Img
+				variant="top"
+				src="https://image.tmdb.org/t/p/w500/7XEtHQoy1hwa8XWaOkSv3rlteea.jpg"
+			/>
 			<Card.Body className="p-2">
 				<Card.Title className="m-0">Card Title</Card.Title>
 			</Card.Body>
